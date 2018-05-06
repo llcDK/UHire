eb32a6dfee83f09fad1257a756c7bb3c18164242
DragonStuff <me@0xdragon.com>
2018-05-06 17:28:12 +1000

    Social login.

---
+++ facebook.php
@@ -0,0 +1,21 @@
+<?php
+    $config = array(
+      "base_url" => "http://127.0.0.1/vendor/hybridauth/",
+      "providers" => array (
+        "Facebook" => array (
+          "enabled" => true,
+          "keys"    => array ( "id" => "164171094252865", "secret" => "16ef9711b9471d45dcfe59f15da94411" ),
+          "scope"   => ['email', 'user_about_me', 'user_birthday', 'user_hometown'], // optional
+          "photo_size" => 200, // optional
+    )));
+ 
+    require_once( "/vendor/hybridauth/Hybrid/Auth.php" );
+ 
+    $hybridauth = new Hybrid_Auth( $config );
+ 
+    $adapter = $hybridauth->authenticate( "Facebook" );
+ 
+    $user_profile = $adapter->getUserProfile();
+    echo "Hi there! " . $user_profile->displayName;
+    echo "Email: " . $user_profile->emailVerified;
+?>
