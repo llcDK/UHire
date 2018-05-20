61e41047a5dc33d61db37d3fc0fdf6f3cd03fccb
chloesoqute <bandjuan1@gmail.com>
2018-05-21 02:45:51 +1000

    finish uploadpage

--- upload.php
+++ upload.php
@@ -3,6 +3,18 @@
 <head>
 	<title>Upload Vehicle</title>
 	<link href="css/upload.css" rel="stylesheet" type="text/css" />
+	<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!-- font -->
+		<link href="http://fonts.googleapis.com/css?family=Kaushan+Script&amp;subset=latin-ext" rel="stylesheet">
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+		<link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		
 </head>
 
 <body>
@@ -100,60 +112,57 @@
 	}
 ?>
 
-<div id="top">
-	<div class="darken">
+	<div class="bigtop">
+			<!--class="darken"-->
+		  <div class="darken">
 
-		<div id="Banner">
-			<div id="topText"> UDRIVE </div>
-		</div>
-
-		<div class="topnav">
-			<a href="main.php">Home</a>
-			<a href="myAccount.php"> My Account</a>
-			<a href="messages.html">Messages</a>
-			<a href="#help"> Help </a>
-			<a href="#contact">Contact</a>
-		</div>
+			 <div id="banner">
+				<div id="topText"> UDRIVE </div>
+			 </div>
+				<div class="topnav">
+		\			<a href="main.php">Home</a>
+					<a href="myAccount.php" class="active"> My Account</a>
+					<a href="messages.html" >Messages</a>
+					<a href="#help"> Help </a>
+					<a href="#contact">Contact</a>
+				</div>
+			</div>
 	</div>
-</div>
+		
+		<div class="padding-100"></div>
+		<div class="tittle"> <h2>.UPLOAD.</h2><div>
+		<div class="padding-100"></div>
 
 
-<div id="pageHeading">
-	<h2>UPLOAD</h2>
-</div>
-<div id="yellowLine">
-	<hr>
-</div>
 
-
-<div id="image">
-	<div id="imgFrame">
+<div class="container">
+	<div class="imgFrame">
 		<img src="images/upload/vehiclePlaceholder.jpg" alt="Upload Image">
 	</div>
 	
-	<div id="uploadDiv">
+	
 		<button id="imgSelect"> Upload Image </button>
-	</div>
+	
 </div>
+
+<div class="padding-100"></div>
 
 <form action = "upload.php?state=1" method = "POST">
 
-	<div id="infoHeadings">
+	<div class="infoHeadings">
 
-		<h2>
-
-			<div>
-				<div class="infoHeading">
-					<span> Plate number </span>
+		<div class="infoHeading">
+					<p> Plate number </p>
 				</div>
 				<div class="infoInput">
 					<input type="text" id="plateNum" name = "pn" value = "<?php echo $_POST['pn']; ?>" />
 				</div>
-			</div>
+				
+				
 			
 			<div>
 				<div class="infoHeading">
-					<span> Price / Day  </span>
+					<p> Price / Day  </p>
 				</div>
 				<div class="infoInput">
 					<input type="text" id="price" name = "pd" value = "<?php echo $_POST['pd']; ?>" />
@@ -163,13 +172,13 @@
 			<!-- Below is a read-only filed -->
 			<div>
 				<div class="infoHeading">
-					<span> Status : Avaiable </span>
+					<p> Status : Avaiable </p>
 				</div>
 			</div>
 			
 			<div>
 				<div class="infoHeading">
-					<span> Location  </span>
+					<p> Location  </p>
 				</div>
 				<div class="infoInput">
 					<input type="text" id="location" name = "loc" value = "<?php echo $_POST['loc']; ?>" />
@@ -179,7 +188,7 @@
 			
 			<div>
 				<div class="infoHeading">
-					<span> Avaiable To  </span>
+					<p> Avaiable To  </p>
 				</div>
 				<div class="infoInput">
 					<input type="date" id="avaiableTo" name = "at" value = "<?php echo $_POST['at']; ?>" />
@@ -189,7 +198,7 @@
 			
 			<div>
 				<div class="infoHeading">
-					<span> Year bought  </span>
+					<p> Year bought  </p>
 				</div>
 				<div class="infoInput">
 					<input type="date" id="year" name = "yb" value = "<?php echo $_POST['yb']; ?>" />
@@ -199,7 +208,7 @@
 			
 			<div>
 				<div class="infoHeading">
-					<span> Model  </span>
+					<p> Model  </p>
 				</div>
 				<div class="infoInput">
 					<input type="text" id="model" name = "m" value = "<?php echo $_POST['m']; ?>" />
@@ -209,7 +218,7 @@
 			
 			<div>
 				<div class="infoHeading">
-					<span> Description  </span>
+					<p> Description  </p>
 				</div>
 				<div class="infoInput">
 					<textarea type="text" id="desc" rows="10" cols="30" name = "desc" value = "<?php echo $_POST['desc']; ?>" > <?php echo $_POST['desc']; ?> </textarea>
@@ -218,7 +227,7 @@
 
 			<div>
 				<div class="infoHeading">
-					<span> Brand </span>
+					<p> Brand </p>
 				</div>
 				<div class="infoInput">
 					<input type="text" id="brand" name = "b" value = "<?php echo $_POST['b']; ?>" />
@@ -227,7 +236,7 @@
 
 			<div>
 				<div class="infoHeading">
-					<span> Transmission  </span>
+					<p> Transmission  </p>
 				</div>
 				<div class="infoInput">
 					<input type="text" id="trans" name = "t" value = "<?php echo $_POST['t']; ?>" />
@@ -236,7 +245,7 @@
 			
 			<div>
 				<div class="infoHeading">
-					<span> Number of Seats  </span>
+					<p> Number of Seats  </p>
 				</div>
 				<div class="infoInput">
 					<input type="text" id="numSeat" name = "ns" value = "<?php echo $_POST['ns']; ?>" />
@@ -245,7 +254,7 @@
 
 			<div>
 				<div class="infoHeading">
-					<span> Odometer  </span>
+					<p> Odometer  </p>
 				</div>
 				<div class="infoInput">
 					<input type="text" id="odo" name = "odo" value = "<?php echo $_POST['odo']; ?>" />
@@ -254,7 +263,7 @@
 			
 			<div>
 				<div class="infoHeading">
-					<span> Fuel Type </span>
+					<p> Fuel Type </p>
 				</div>
 				<div class="infoInput">
 					<input type="text" id="fuel" name = "ft" value = "<?php echo $_POST['ft']; ?>" />
@@ -263,7 +272,7 @@
 
 			<div>
 				<div class="infoHeading">
-					<span> Body Type  </span>
+					<p> Body Type  </p>
 				</div>
 				<div class="infoInput">
 					<input type="text" id="body" name = "bt" value = "<?php echo $_POST['bt']; ?>" />
@@ -280,6 +289,27 @@
 	</h2>
 	</div>
 </form>
+
+<div class="padding-100"></div>
+	<footer>
+		<div class="copyright">
+	    <div class="line">
+			<div class="margin">
+				<!-- left -->
+				<div class="s-12 m-12 l-8 footer-links">
+					<span>Copyright &copy; 2018.Designed by WENJUANSUAN.</span>
+				</div>
+				<!-- right -->
+				<div class="s-12 m-12 l-4 payment-methods">
+					<i class="fa fa-cc-visa fa-2x"></i>
+					<i class="fa fa-cc-mastercard fa-2x"></i> 
+					<i class="fa fa-cc-paypal fa-2x"></i>
+					<i class="fa fa-credit-card fa-2x"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
 
 </body>
 
